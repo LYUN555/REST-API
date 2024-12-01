d83cb05b345eb96fce8fec5a920c450019d9ae3e
Lyun555 <askaskj3@gmail.com>
2024-12-01 17:38:26 +0900

    jpa 연결

--- src/main/java/com/lyun/rest/webservices/restfulwebservices/filtering/FilteringController.java
+++ src/main/java/com/lyun/rest/webservices/restfulwebservices/filtering/FilteringController.java
@@ -19,12 +19,7 @@ public class FilteringController {
 		SomeBean someBean = new SomeBean("value1","value2","value3");
 		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(someBean);
 		
-		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field1","field3");
-		
-		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter );
-		
-		mappingJacksonValue.setFilters(filters);
-		
+		SimpleBeanPropertyFilter(mappingJacksonValue);
 		return mappingJacksonValue;
 	}
 	
@@ -35,9 +30,13 @@ public class FilteringController {
 				new SomeBean("value4","value5","value6"));
 		MappingJacksonValue mappingJacksonValue = new MappingJacksonValue(asList);
 		
+		SimpleBeanPropertyFilter(mappingJacksonValue);
+		return mappingJacksonValue;
+	}
+
+	private void SimpleBeanPropertyFilter(MappingJacksonValue mappingJacksonValue) {
 		SimpleBeanPropertyFilter filter = SimpleBeanPropertyFilter.filterOutAllExcept("field2","field3");
 		FilterProvider filters = new SimpleFilterProvider().addFilter("SomeBeanFilter", filter );
 		mappingJacksonValue.setFilters(filters);
-		return mappingJacksonValue;
 	}
 }
---
+++ src/main/java/com/lyun/rest/webservices/restfulwebservices/jpa/UserRepository.java
@@ -0,0 +1,9 @@
+package com.lyun.rest.webservices.restfulwebservices.jpa;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.lyun.rest.webservices.restfulwebservices.user.User;
+
+public interface UserRepository extends JpaRepository<User, Integer>{
+
+}
