8cbbc2ec9b393f33cf108ce11ac0aaecf17be285
Lyun555 <askaskj3@gmail.com>
2024-12-09 21:46:12 +0900

    Change-Id: I0000000000000000000000000000000000000000
    Change-Id: I2d3e5fca78b13bce24d3c0febc9ff183b65f13b4

--- src/main/java/com/lyun/rest/webservices/restfulwebservices/user/PostNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.lyun.rest.webservices.restfulwebservices.user;
-
-public class PostNotFoundException extends RuntimeException {
-	public PostNotFoundException(String msg) {
-		super(msg);
-	}
-}
